98ef524d82c5a1865ca32bbdf966622d687dd4bc
Etienne Lomet <tino.tablette@gmail.com>
2022-05-15 03:07:16 +0200

    v1

--- main.py
+++ main.py
@@ -1,94 +1,195 @@
 
 from discord.ext import commands
 import redis
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 bot = commands.Bot(command_prefix='!')
 
 rdb = redis.Redis()
 
-mmr_decay = 3
-mmr_workout = 10
-mmr_active = 3
+MMR_BASE = 1000
+MMR_DECAY = 3
+MMR_WORKOUT = 10
+MMR_ACTIVE = 3
+
+
+@bot.command()
+async def test(ctx, week_day=None):
+    print(week_day)
 
 
 @bot.command()
 async def mmr(ctx):
     
-    id = str(ctx.author.id)
-    name = ctx.author.name
+    musclor = find_musclor(ctx, None)
 
-    if not rdb.sismember('musclorz-ids', id):
-        new_musclor(id, name)
-
-    update_mmr(id)
-
-    await ctx.send(name + ' you are a musclor' + \
-        rdb.hget('musclor:' + id + ':info', 'mmr').decode())
+    await ctx.send(ctx.author.name + " you're a musclor" + \
+        rdb.hget(musclor + ':info', 'mmr').decode())
 
 
 @bot.command()
-async def workout(ctx):
+async def workout(ctx, week_day=None):
     
-    id = str(ctx.author.id)
-    name = ctx.author.name
-
-    if not rdb.sismember('musclorz-ids', id):
-        new_musclor(id, name)
-
-    rdb.lpush('musclor:' + id + ':workouts', str(datetime.now().date()))
-    rdb.hincrby('musclor:' + id + ':info', 'mmr', mmr_workout)
-
-    update_mmr(id)
+    musclor = find_musclor(ctx, week_day)
+    
+    n_day = day_number(musclor, determine_date(week_day))
+    
+    rdb.lpush(musclor + ':workouts', n_day)
+    rdb.hincrby(musclor + ':info', 'mmr', MMR_WORKOUT)
+    rdb.hset(musclor + ':info', 'pause', 'off')
 
     await ctx.message.add_reaction('💪')
 
 
 @bot.command()
-async def active(ctx):
+async def active(ctx, week_day=None):
 
-    id = str(ctx.author.id)
-    name = ctx.author.name
+    musclor = find_musclor(ctx, week_day)
+    
+    n_day = day_number(musclor, determine_date(week_day))
 
-    if not rdb.sismember('musclorz-ids', id):
-        new_musclor(id, name)
-
-    rdb.lpush('musclor:' + id + ':active-rests', str(datetime.now().date()))
-    rdb.hincrby('musclor:' + id + ':info', 'mmr', mmr_active)
-
-    update_mmr(id)
+    rdb.lpush(musclor + ':actives', n_day)
+    rdb.hincrby(musclor + ':info', 'mmr', MMR_ACTIVE)
+    rdb.hset(musclor + ':info', 'pause', 'off')
 
     await ctx.message.add_reaction('🧘')
+    
+    
+@bot.command()
+async def pause(ctx):
+    
+    musclor = find_musclor(ctx, None)
+    
+    rdb.hset(musclor + ':info', 'pause', 'on')
+    
+    await ctx.message.add_reaction('🛌')
+    
+
+@bot.command()
+async def achievment(ctx, arg):
+    
+    musclor = find_musclor(ctx, None)
+    
+    rdb.lpush(musclor + ':achievments', arg)
+    
+    await ctx.message.add_reaction('🏆')
+    
+
+@bot.command()
+async def achievments(ctx):
+    
+    musclor = find_musclor(ctx, None)
+    
+    achievments_list = rdb.lrange(musclor + ':achievments', 0, -1)
+    
+    if len(achievments_list) == 0:
+        msg = ctx.author.name + ' has no achievments yet'
+    else:
+        msg = ctx.author.name + ' has the following achievments: ' \
+            + ', '.join([achv.decode() for achv in achievments_list])
+            
+    await ctx.send(msg)
+    
+    
+def find_musclor(ctx, week_day):
+    
+    discord_id = ctx.author.id
+    
+    id = rdb.zscore('musclorz_ids', discord_id)
+    
+    if id == None:
+        musclor = new_musclor(discord_id, ctx.author.name, week_day)
+    else:
+        musclor = 'musclor:' + str(int(id))
+        
+    update_mmr(musclor)
+        
+    return musclor
 
 
-def new_musclor(id, name):
+def new_musclor(discord_id, name, week_day):
+    
+    id = rdb.incr('n_musclorz')
 
-    rdb.sadd('musclorz-ids', id)
+    rdb.zadd('musclorz_ids', {discord_id: id})
+    
+    musclor = 'musclor:' + str(id)
 
-    current_date_str = str(datetime.now().date())
-    rdb.hset('musclor:' + id + ':info', None, None, {
+    date_str = str(determine_date(week_day))
+    rdb.hset(musclor + ':info', None, None, {
         'id': id,
+        'discord_id': discord_id,
         'name': name,
-        'start-date': current_date_str,
-        'mmr': 1000,
-        'last-update': current_date_str,
+        'start_date': date_str,
+        'mmr': MMR_BASE,
+        'last_update': date_str,
+        'pause': 'off',
     })
+    
+    return musclor
 
 
-def update_mmr(id):
+def update_mmr(musclor):
+    
     current_date = datetime.now().date()
-    last_update = datetime.strptime(
-        rdb.hget('musclor:' + id + ':info', 'last-update').decode(), '%Y-%m-%d'
-    ).date()
+    last_update = decode_date(rdb.hget(musclor + ':info', 'last_update'))
+    
     n_days = (current_date - last_update).days
-    mmr = int(rdb.hget('musclor:' + id + ':info', 'mmr')) - mmr_decay * n_days
-    if mmr < 0:
-        mmr = 0
-    rdb.hset('musclor:' + id + ':info', None, None, {
+    mmr = int(rdb.hget(musclor + ':info', 'mmr'))
+    
+    pause = rdb.hget(musclor + ':info', 'pause') == 'on'
+    if not pause:
+        mmr -= MMR_DECAY * n_days
+        if mmr < 0:
+            mmr = 0
+        
+    rdb.hset(musclor + ':info', None, None, {
         'mmr': mmr,
-        'last-update': str(current_date),
+        'last_update': str(current_date),
     })
+
+
+def day_number(musclor, date):
+    
+    start_date = decode_date(rdb.hget(musclor + ':info', 'start_date'))
+    
+    return (date - start_date).days
+    
+
+def decode_date(date):
+    return datetime.strptime(date.decode(), '%Y-%m-%d').date()
+
+
+def determine_date(week_day):
+    
+    today = datetime.now().date()
+    
+    if week_day == None:
+        return today
+    
+    week_day = week_day.lower()
+    
+    if week_day == 'lundi' or week_day == 'monday':
+        n_weekday = 0
+    elif week_day == 'mardi' or week_day == 'tuesday':
+        n_weekday = 1
+    elif week_day == 'mercredi' or week_day == 'wednesday':
+        n_weekday = 2
+    elif week_day == 'jeudi' or week_day == 'thursday':
+        n_weekday = 3
+    elif week_day == 'vendredi' or week_day == 'friday':
+        n_weekday = 4
+    elif week_day == 'samedi' or week_day == 'saturday':
+        n_weekday = 5
+    elif week_day == 'dimanche' or week_day == 'sunday':
+        n_weekday = 6
+    else:
+        return today
+    
+    days_diff = (today.weekday() - n_weekday + 7) % 7
+    
+    return today - timedelta(days_diff)
 
 
 with open('token') as token_file:
